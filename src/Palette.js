dae795919a8d9ab07079505b21ce0fc7bba84924
KyleDylan <55897087+KyleDylan@users.noreply.github.com>
2019-11-19 18:59:58 -0800

    'halfway'

---
+++ src/Palette.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
+import NavBar from './NavBar';
+
+class Palette extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500, format: 'hex' };
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
+    }
+
+    changeLevel(level) {
+        this.setState({ level });
+    }
+
+    changeFormat(val) {
+        this.setState({ format: val });
+    }
+
+    render() {
+        const { colors, paletteName, emoji } = this.props.palette;
+        const { level, format } = this.state; 
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color[format]} name={color.name} key={color.id} />
+        ));
+        return(
+            <div className='Palette'>
+            <NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
+                <div className='Palette-colors'>
+                    {colorBoxes}
+                </div>
+                <footer className='Palette-footer'>
+                    {paletteName}
+                    <span className='emoji'>{emoji}</span>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Palette;
